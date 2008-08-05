7d4a4620155763cdf31619e49e8e8c16c0094f73
marc.lanctot <marc.lanctot@8b1a7c45-654f-0410-8cbb-f509d8e73029>
2008-08-05 00:04:59 +0000

    ...

--- branches/rlglue/src/ai/module.mk
+++ branches/rlglue/src/ai/module.mk
@@ -14,23 +14,6 @@ MODULE_OBJS := \
 	src/ai/networking/rl-glue/RL_network_environment.o \
 	src/ai/networking/rl-glue/RL_network.o 
 
-#	Removed these.. they're only used in the direct connection
-#	src/ai/networking/rl-glue/RL_network.o \
-#	src/ai/networking/rl-glue/RL_network_glue.o \
-#
-#
-#	src/ai/networking/rl-glue/RL_client_agent.o \
-#	src/ai/networking/rl-glue/RL_client_experiment.o \
-#	src/ai/networking/rl-glue/RL_direct_agent.o \
-#	src/ai/networking/rl-glue/RL_direct_environment.o \
-#	src/ai/networking/rl-glue/RL_direct_experiment.o \
-#	src/ai/networking/rl-glue/RL_network_experiment.o \
-#	src/ai/networking/rl-glue/RL_network_agent.o \
-#	src/ai/networking/rl-glue/RL_server_agent.o \
-#	src/ai/networking/rl-glue/RL_server_experiment.o \
-#	src/ai/networking/rl-glue/RL_glue.o \
-#	src/ai/networking/rl-glue/RL_server_environment.o \
-
 MODULE_DIRS += \
 	src/ai
 
