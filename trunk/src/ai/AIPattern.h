39824196f908eb996e0cac2b7ba4d5a6d9681308
marc.lanctot <marc.lanctot@8b1a7c45-654f-0410-8cbb-f509d8e73029>
2008-09-25 04:33:30 +0000

    ...

--- trunk/src/ai/AIPattern.h
+++ trunk/src/ai/AIPattern.h
@@ -4,10 +4,11 @@
 
 #include <vector>
 
+#define CROSSTRIPLETS 0
+
 typedef std::pair< std::pair<int,int> , int > ptriplet; 
 
 typedef std::vector< std::vector<int> > pattern; 
-
 
 /* A pattern is a shape of pixels on the screen */
 
@@ -32,6 +33,37 @@ public:
   void set_val(int row, int col, int color); 
 };
 */
+
+#if CROSSTRIPLETS
+
+typedef std::pair< std::pair< std::pair<int,int>, std::pair<int,int> > , int > pcrosstriplet; 
+
+#include <ext/hash_map>
+//#include <unordered_map>
+#include <limits.h>
+
+struct crosstriplet { int x1, y1, color1, x2, y2, color2; }; 
+
+struct ctHash {
+  size_t operator()(const crosstriplet &ct) const
+  {
+    return (ct.x1 ^ ct.y1 ^ ct.color1 ^ ct.x2 ^ ct.y2 ^ ct.color2); 
+  }
+};
+
+struct ctEqual {
+  bool operator()(const crosstriplet & ct1, const crosstriplet & ct2)
+  { 
+    return (   ct1.x1 == ct2.x1 && ct1.x2 == ct2.x2
+            && ct1.y1 == ct2.y1 && ct1.y2 == ct2.y2
+            && ct1.color1 == ct2.color1 && ct1.color2 == ct2.color2); 
+  }
+};
+
+typedef __gnu_cxx::hash_map<crosstriplet, bool, ctHash, ctEqual > ct2boolmap; 
+typedef __gnu_cxx::hash_map<int,int> int2intmap; 
+
+#endif 
 
 #endif
 
