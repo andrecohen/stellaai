ca3fd8e63b144812352950c11141f889db53f738
marc.lanctot <marc.lanctot@8b1a7c45-654f-0410-8cbb-f509d8e73029>
2008-10-24 17:03:08 +0000

    RL-Glue 3 branch initial commit

---
+++ branches/rlglue3/src/ai/module.mk
@@ -0,0 +1,26 @@
+MODULE := src/ai
+
+MODULE_OBJS := \
+	src/ai/AIBase.o \
+	src/ai/AIPattern.o \
+	src/ai/AIRewards.o \
+	src/ai/AIScript.o \
+	src/ai/AISettings.o \
+	src/ai/networking/rl-glue/RLglue_codec.o \
+	src/ai/networking/AIGlue.o \
+  src/ai/networking/AIGlueEnv.o \
+  src/ai/networking/AIProtocol.o \
+	src/ai/networking/AIPlainText.o \
+	src/ai/networking/AISocket.o 
+#	src/ai/networking/rl-glue/C_TaskSpec_Parser.o \
+#	src/ai/networking/rl-glue/RLStruct_util.o \
+#	src/ai/networking/rl-glue/RL_network_environment.o \
+#	src/ai/networking/rl-glue/RL_network.o 
+
+MODULE_DIRS += \
+	src/ai
+
+LIBS += -llua -lrlgluenetdev 
+
+# Include common rules 
+include $(srcdir)/common.rules
