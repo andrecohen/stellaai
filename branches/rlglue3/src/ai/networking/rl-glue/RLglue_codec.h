60453582912ca25d336be34b37236186f5071ae1
andre.cohen <andre.cohen@8b1a7c45-654f-0410-8cbb-f509d8e73029>
2008-11-02 17:29:55 +0000

    Made it compatible with Macs

--- branches/rlglue3/src/ai/networking/rl-glue/RLglue_codec.h
+++ branches/rlglue3/src/ai/networking/rl-glue/RLglue_codec.h
@@ -2,9 +2,17 @@
 #ifndef __RLGLUE_CODEC_H
 #define __RLGLUE_CODEC_H
 
+#if __cplusplus
+extern "C" {
+#endif
+
 int rlStartEnvironment(char *host, int port);
 void rlUpdateEnvironment();
 void rlEndEnvironment();
 
+#if __cplusplus
+}
+#endif
+	
 #endif
 
