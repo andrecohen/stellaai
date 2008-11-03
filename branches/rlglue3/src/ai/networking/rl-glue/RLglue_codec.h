f2761abcf2dbd690a38429d4bfed4d4f2168f301
marc.lanctot <marc.lanctot@8b1a7c45-654f-0410-8cbb-f509d8e73029>
2008-11-03 16:48:14 +0000

    Added an ifdef for MacOS X

--- branches/rlglue3/src/ai/networking/rl-glue/RLglue_codec.h
+++ branches/rlglue3/src/ai/networking/rl-glue/RLglue_codec.h
@@ -2,16 +2,20 @@
 #ifndef __RLGLUE_CODEC_H
 #define __RLGLUE_CODEC_H
 
+#ifdef __APPLE__
 #if __cplusplus
 extern "C" {
+#endif
 #endif
 
 int rlStartEnvironment(char *host, int port);
 void rlUpdateEnvironment();
 void rlEndEnvironment();
 
+#ifdef __APPLE__
 #if __cplusplus
 }
+#endif
 #endif
 	
 #endif
